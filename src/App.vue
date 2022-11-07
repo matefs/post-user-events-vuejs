ce23fde4e518ed9f5ebb11301504cfd4756d83a2
Mateus Schverz <mate_fs@hotmail.com>
2022-11-07 14:13:06 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='shopping-list'>
+    <h1>{{ header.toUpperCase() ?? 'Welcome' }}</h1>
+  
+
+
+  <div class='add-item-form'>
+ 
+
+<input type='text' v-model='newItem' placeholder="Add an item" @keyup.enter="addItemToItemsList(newItem)">
+  
+  <button
+  @click="addItemToItemsList(newItem)"
+  class="btn btn-primary"
+  >Add item
+  </button>
+
+  </div>
+  
+  <ul>
+    <li v-for="item in itemsList" :key='item.id'>{{item.label}}</li>
+  </ul>
+
+
+  <button @click='postItemsListObjectAsJson'>Update List</button>
+  
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+ 
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      header: 'Shopping item list ',
+      newItem: '',
+      itemsList: [
+      {id:1 , label: 'Milk'},
+      {id:2 , label: 'Rice'},
+      {id:3 , label: 'Beans'},
+      ]
+    }
+  },
+  methods:{
+      addItemToItemsList: function(newItem){
+        this.itemsList.push({id: this.itemsList.length + 1 , label: newItem})
+        console.table(this.itemsList)
+      },
+      postItemsList : function(ItemsListJson){
+        axios.post('https://mateus.requestcatcher.com/',ItemsListJson).then(response => console.log(response))
+      },
+      postItemsListObjectAsJson : function(){
+        return this.postItemsList(JSON.stringify(this.itemsList))
+      }
+    }
+   
 }
 </script>
 
